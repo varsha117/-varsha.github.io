960f13020d86152dfec563d7173cf64f7dfee5ab
Varsha B <varshabreddy17@gmail.com>
2021-06-02 19:37:44 +0530

    CV links

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Blogger", "Designer"],
+        strings: [ "Student", "Programmer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
