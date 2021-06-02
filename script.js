4ea4ab381bca89b8f9ed3c3e3282da200172c828
Varsha B <varshabreddy17@gmail.com>
2021-06-02 20:01:00 +0530

    about me

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developer", "Blogger", "Designer"],
+        strings: [ "Student", "Programmer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
